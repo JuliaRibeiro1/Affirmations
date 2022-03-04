61beffbaaa83b3026f781c9fdb62687c1f5c35ca
Julia <hijulia259@gmail.com>
2022-03-04 09:34:13 -0300

    falta css

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+/*const introBtn = document.querySelector(".IcanBtn").addEventListener('click', () => {
+    alert('oi')
+})
+
+/*const introBtnFunction = introBtn.addEventListener('click', blabla)
+function  blabla() {
+    alert('oi')
+}
+
+function blabla() {
+    introBtn.style.display = "none"
+}
+setTimeout(blabla,2000)
+*/ 
+
+
